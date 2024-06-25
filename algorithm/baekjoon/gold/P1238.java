b57d785182f74475f03a5fc772fb9d4d82a34bd2
MoonChangHo <tony9911@naver.com>
2024-06-25 18:02:17 +0900

    [Sol] ✍ - 파티(다익스트라) [g3]
    #dijkstra
    #문제번호 : 1238
    #memory : 19824KB
    #time : 224ms
    
    [문제]
    https://www.acmicpc.net/problem/1238
    
    [풀이]
    - N->X->N을 구해야 하는데, 맵을 그릴 때 반전 시킨 맵을 하나 더 그리는게 핵심이다.

--- algorithm/baekjoon/gold/P1238.java
+++ algorithm/baekjoon/gold/P1238.java
@@ -1,61 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P1238 {
-    //N명 학생
-    // X번 마을 에 모여
-    // M 개의 단방향 도로
-    // i번째 길이를 지나는데 Ti시간 소비
-    // N명의 학생 중 누가 가장 많은 시간을 소비하냐
-
     static int N,M,X;
-//    static List<Edge> map;
-    static int[][] map;
+    static List<Edge>[] map;
+    static List<Edge>[] reverseMap;
+
+    static class Edge{
+        int to, w;
+        Edge(int to,int w){
+            this.to=to;
+            this.w=w;
+        }
+    }
     static void input() throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        N= Integer.parseInt(st.nextToken());    //학생 수
-        M= Integer.parseInt(st.nextToken());    //도로 개수
-        X= Integer.parseInt(st.nextToken())-1;    //모일 마을
-        map=new int[N][N];
-        for(int i=0;i<N;i++) {
-            Arrays.fill(map[i], 999999999);
-            map[i][i]=0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());    //학생 수
+        M = Integer.parseInt(st.nextToken());    //도로 개수
+        X = Integer.parseInt(st.nextToken()) - 1;    //모일 마을
+
+        map=new LinkedList[N];
+        reverseMap=new LinkedList[N];
+
+        for(int i=0;i<N;i++){
+            map[i]=new LinkedList<>();
+            reverseMap[i]=new LinkedList<>();
         }
 
-        for(int i=0;i<M;i++){
-            st=new StringTokenizer(br.readLine());
-            int a= Integer.parseInt(st.nextToken())-1;
-            int b= Integer.parseInt(st.nextToken())-1;
-            int w= Integer.parseInt(st.nextToken());
-            map[a][b]=w;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+            map[a].add(new Edge(b,w));
+            reverseMap[b].add(new Edge(a,w));
         }
-        floydwarshall();
+
+        int[] rdist=dijkstra(reverseMap,X);
+        int[] dist=dijkstra(map,X);
 
         int max=Integer.MIN_VALUE;
         for(int i=0;i<N;i++){
-            max=Math.max(max,map[i][X]+map[X][i]);
+            int n=rdist[i]+dist[i];
+            max=Math.max(n,max);
         }
         System.out.println(max);
     }
 
-    static void floydwarshall(){
+    static int[] dijkstra(List<Edge>[] map,int start){
+        boolean[] visit=new boolean[N];
+        int[] dist=new int[N];
+        Arrays.fill(dist,999999999);
 
-        //거칠 노드
-        for(int i=0;i<N;i++){
-            //시작 노드
-            for(int j=0;j<N;j++){
-                //종료 노드
-                for(int k=0;k<N;k++){
-                    if(map[j][k]>map[j][i]+map[i][k]){
-                        map[j][k]=map[j][i]+map[i][k];
-                    }
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        pq.offer(new int[]{start,0});
+        dist[start]=0;
+
+        while(!pq.isEmpty()){
+            int[] arr =pq.poll();
+            if(visit[arr[0]]) continue;
+            visit[arr[0]]=true;
+
+            for(Edge edge:map[arr[0]]){
+                if(dist[edge.to]>dist[arr[0]]+ edge.w){
+                    dist[edge.to]=dist[arr[0]]+ edge.w;
+                    pq.offer(new int[]{edge.to,dist[edge.to]});
                 }
             }
         }
+        return dist;
     }
 
     public static void main(String[] args) throws IOException {
